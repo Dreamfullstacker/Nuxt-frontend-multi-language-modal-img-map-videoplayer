eae21782c51c49ac60d08e9fe77c898efde9e39a
Dreamfullstacker <97073529+Dreamfullstacker@users.noreply.github.com>
2022-05-24 20:27:15 +1000

    change mute/unmute play/pause icon

--- static/Images/c1/sectionOne/Modal.vue
+++ static/Images/c1/sectionOne/Modal.vue
@@ -35,6 +35,7 @@
                 <div class="absolute bottom-0 z-40 mb-10 mr-5">
                 <div v-if="InsideFrameIndia" class="z-50 flex flex-row items-center">
                     <svg
+                      v-if="!mute"
                         @click="muteIndia"
                         :class="Indiarepeat ? 'block' : 'hidden'"
                         class="w-12 ml-4 text-white bg-transparent border border-transparent rounded cursor-pointer fill-current hover:text-black hover:shadow hover:bg-gray-100"
@@ -46,17 +47,34 @@
                             d="M21.707 20.293l-2.03-2.03c1.44-1.686 2.31-3.846 2.31-6.27 0-4.091-2.48-7.453-6-9v2c2.38 1.38 3.99 4.04 3.99 7 0 1.83-.63 3.54-1.68 4.913L17 15.616c.6-1.1.95-2.44.95-3.628 0-1.78-.78-3.9-2-5v7.586l-2-2V3.98c0-.37-.21-.71-.53-.89 -.33-.18-.72-.16-1.03.05L7.67 6.29 3.65 2.27l-1.42 1.41 18 18 1.41-1.42ZM12 5.86v4.718L9.16 7.74l2.83-1.89ZM4 16.99h2.697l5.748 3.83c.16.11.36.16.55.16 .16 0 .32-.04.47-.12 .32-.18.52-.52.52-.89v-1.88l-2-2v2.01L7.54 15.13c-.03-.02-.06-.02-.09-.04 -.07-.04-.13-.06-.19-.08 -.07-.02-.13-.04-.2-.04 -.04-.01-.06-.02-.1-.02h-3v-6h.87L3.05 7.17c-.66.33-1.11.99-1.11 1.77v6c0 1.1.89 2 2 2Z"
                         />
                     </svg>
-                    <svg
-                        @click="playstopIndia"
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        v-if="mute"
+                        @click="muteIndia"
                         :class="Indiarepeat ? 'block' : 'hidden'"
                         class="w-12 ml-4 text-white bg-transparent border border-transparent rounded cursor-pointer fill-current hover:text-black hover:shadow hover:bg-gray-100"
                         version="1.1"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <polygon class="play-btn__svg" points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69"/>
-                        <path class="play-btn__svg" d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z"/>
-                         
+                        viewBox="0 0 16 16"> 
+                      <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/> 
+                      <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/> 
+                      <path d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z"/> 
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                      v-if="playpause"
+                      @click="playstopIndia"
+                      :class="Indiarepeat ? 'block' : 'hidden'"
+                      class="w-12 ml-4 text-white bg-transparent border border-transparent rounded cursor-pointer fill-current hover:text-black hover:shadow hover:bg-gray-100"
+                      viewBox="0 0 16 16"> 
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/> 
+                      <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/> 
+                    </svg>
+                    <svg 
+                      v-if="!playpause"
+                      @click="playstopIndia"
+                      :class="Indiarepeat ? 'block' : 'hidden'"
+                      class="w-12 ml-4 text-white bg-transparent border border-transparent rounded cursor-pointer fill-current hover:text-black hover:shadow hover:bg-gray-100"
+                      viewBox="0 0 16 16"> 
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path> 
+                      <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5z"></path> 
                     </svg>
 
                     <svg
@@ -134,6 +152,7 @@ export default {
   name: "Modal",
   data() {
     return {
+      mute : false,
       show: false,
       playpause : true,
       Indiaaudio: true,
@@ -177,6 +196,7 @@ export default {
     muteIndia() {
       setTimeout(() => {
         this.$refs.india.muted = !this.$refs.india.muted
+        this.mute = !this.mute
       }, 500)
     },
     playstopIndia() {
